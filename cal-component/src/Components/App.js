9998680cf76ca1e996ea09ffc46df3192f9e2f52
fomekongsandra <63794104+fomekongsandra@users.noreply.github.com>
2022-12-14 14:15:15 +0100

    ajout du dossier Asset
    j'ai cree le composant calheader

--- cal-component/src/Components/App.js
+++ cal-component/src/Components/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import CalHeader from './CalHeader '
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           Hello Prof
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        React documentation
-        </a>
-      </header>
+     <CalHeader/>
     </div>
   );
 }
